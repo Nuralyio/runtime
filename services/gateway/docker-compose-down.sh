7f9185d07dd179b1818e1d2efee5360ab52e036f
Aymen <labidi@aymen.co>
2024-01-03 22:24:53 +0100

    Update docker-compose command in docker-compose-down.sh

--- services/gateway/docker-compose-down.sh
+++ services/gateway/docker-compose-down.sh
@@ -1,5 +1,5 @@
-if docker-compose ps | grep -q 'Up'; then
-    docker-compose down
+if docker compose ps | grep -q 'Up'; then
+    docker compose down
 else
     echo "No containers are currently running."
 fi
