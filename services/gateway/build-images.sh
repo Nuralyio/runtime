5f403a44d3d0c43c337582d43b9935c304d269c9
Aymen <labidi@aymen.co>
2024-01-05 17:15:41 +0100

    Add cache directory and configuration file for Keycloak

--- services/gateway/build-images.sh
+++ services/gateway/build-images.sh
@@ -1,2 +1,2 @@
-docker build ./Keycloack --file ./Keycloack/Dockerfile --tag nuraly/keycloak
+docker build ./Keycloack --file ./Keycloack/Dockerfile --tag nuraly/keycloack
 docker build ./Nginx --file ./Nginx/Dockerfile --tag nuraly/nginx
