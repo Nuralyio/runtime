966263052d405107754b59783ec0a7f9e9e19a04
Aymen <labidi@aymen.co>
2024-02-11 07:02:57 +0100

    refactor font styles

--- services/studio/src/components/pages/studio/ControlPanel/TextAttributes/FontStyle/FontStyle.ts
+++ services/studio/src/components/pages/studio/ControlPanel/TextAttributes/FontStyle/FontStyle.ts
@@ -2,46 +2,42 @@ import { updateComponentAttributes } from "$store/component/action";
 import { type ComponentElement } from "$store/component/interface";
 import { LitElement, html, css, nothing } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import "./FontStyleValue/FontStyleValue";
-import "./FontStyleLabel/FontStyleLabel";
+import "./FontStyleHandler";
+
 @customElement("attribute-text-font-style")
 export class FOntSTyleATtribute extends LitElement {
   @property({ type: Object })
   component: ComponentElement;
   @property({ type: Boolean })
   slim: boolean = false;
-  
+
   static styles = [
-    css`
-      .container {
-        display: flex;
-        flex-direction: row;
-      }
-      .first_column {
-        width: 40%;
-      }
-    `,
+    css``,
   ];
 
   changeHandler(event: CustomEvent) {
-    if (event.detail.value === "italic") {
-      updateComponentAttributes(this.component.uuid, {
-        textDecoration: "",
-        fontStyle: "italic",
-      });
-    }
+    
+    const {
+      detail: { value },
+    } = event;
+
     updateComponentAttributes(this.component.uuid, {
-      textDecoration: event.detail.value,
+      textDecoration: value.textDecoration,
+      fontStyle: value.fontStyle,
     });
+   
   }
 
   render() {
-    return html` <div class="container">
-      ${this.slim ? nothing : html`<attribute-font-style-value-label class="first_column"></attribute-font-style-value-label>` }
+    return html` 
+    <editpanel-attribute-container>
+      <span  slot="firstColumn">Font style</span>
       <attribute-font-style-value-handler
+        slot="secondColumn"
         .component=${{ ...this.component }}
         @attributeUpdate=${this.changeHandler}
       ></attribute-font-style-value-handler>
-    </div>`;
+    </editpanel-attribute-container>
+`;
   }
 }
