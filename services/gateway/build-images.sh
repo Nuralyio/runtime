8639c8ba33db7dc9431bbbf7b312cb88a5c08d05
Aymen <labidi@aymen.co>
2024-04-14 01:15:23 +0100

    Refactor Docker-related scripts and configurations

--- services/gateway/build-images.sh
+++
@@ -1,2 +0,0 @@
-docker build ./Keycloack --file ./Keycloack/Dockerfile --tag nuraly/keycloack
-docker build  --build-arg DOMAIN=$HOST_DOMAIN ./Nginx --file ./Nginx/Dockerfile --tag nuraly/nginx 
--- services/gateway/docker-compose-down.sh
+++
@@ -1,6 +0,0 @@
-source ~/.bashrc
-if docker compose ps | grep -q 'Up'; then
-    docker compose down
-else
-    echo "No containers are currently running."
-fi
