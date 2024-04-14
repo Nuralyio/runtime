452d3b669db6a066d72350c9f5156a057363272f
Aymen <labidi@aymen.co>
2024-04-14 01:03:16 +0100

    Update NURALY_SERVICES_HOST in constants.ts to use localhost instead of nuraly.io

--- services/studio/src/services/constants.ts
+++ services/studio/src/services/constants.ts
@@ -1,7 +1,7 @@
 export const APIS_URL = {
-    getApplication: (id) => `http://${process.env.NURALY_SERVICES_HOST || "nuraly.io"}/api/applications/${id}`,
-    getApplications:()=> `http://${process.env.NURALY_SERVICES_HOST || "nuraly.io"}/api/applications`,
-    getApplicationPages: (uuid) => `http://${process.env.NURALY_SERVICES_HOST || "nuraly.io"}/api/pages/${uuid}`,
-    getPageComponents: (uuid) => `http://${process.env.NURALY_SERVICES_HOST || "nuraly.io"}/api/components/${uuid}`,
+    getApplication: (id) => `http://${process.env.NURALY_SERVICES_HOST || "localhost"}/api/applications/${id}`,
+    getApplications:()=> `http://${process.env.NURALY_SERVICES_HOST || "localhost"}/api/applications`,
+    getApplicationPages: (uuid) => `http://${process.env.NURALY_SERVICES_HOST || "localhost"}/api/pages/${uuid}`,
+    getPageComponents: (uuid) => `http://${process.env.NURALY_SERVICES_HOST || "localhost"}/api/components/${uuid}`,
     getApplicationPermission: (application_id: string, resource_id: string) => `/api/permissions/${application_id}/${resource_id}`,
 }
