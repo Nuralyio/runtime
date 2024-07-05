f7fb941af41bae8d7477692601d786034e4959ce
mdevuser1 <labidi@aymen.co>
2024-07-05 16:34:04 +0100

    Update hy-checkbox component (#6)
    * Update hy-checkbox component
    
    * Enhance UI

--- components/ui/nuraly-ui/src/components/checkbox/checkbox.style.ts
+++ components/ui/nuraly-ui/src/components/checkbox/checkbox.style.ts
@@ -1,64 +1,94 @@
-import { css } from "lit";
+import {css} from 'lit';
+import {styleVariables} from './checkbox.variables';
 
-export default css`
-:host {
-      display: inline-block;
-    }
-    div {
-      display: flex;
-      align-items: center;
-      cursor: pointer;
-    }
-    div.disabled {
-      opacity: 0.5;
-      cursor: auto;
-    }
-    .checkbox {
-      display: flex;
-      background-color: var(--hy-checkbox-checked-color, #30a030);
-      border-radius: 4px;
-      width: var(--hy-checkbox-size, 20px);
-      height: var(--hy-checkbox-size, 20px);
-      line-height: 0;
-      align-items: center;
-      justify-content: center;
-    }
-    .isUnchecked {
-      background-color: var(--hy-checkbox-unchecked-color, #ccc);
-    }
-    path[fill="none"], .checked path[fill="none"] {
-      fill: transparent;
-    }
-    path {
-      fill: var(--hy-checkbox-fill-color, #fff);
-    }
-    .checked path {
-      fill: var(--hy-checkbox-unchecked-fill-color, #fff);
-    }
-    svg {
-      width: var(--hy-checkbox-size, 20px);
-      height: var(--hy-checkbox-size, 20px);
-      line-height: var(--hy-checkbox-size, 20px);
-    }
-    .label {
-      margin-left: 10px;
-      font-weight: var(--hy-checkbox-font-weight, normal);
-      color: var(--hy-checkbox-label-color, #303030);
-    }
-    .disabled .label{
-      color: var(--hy-checkbox-label-disabled-color, #303030);
-    }
+const checkBoxStyles = css`
+  :host {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    gap: var(--hybrid-checkbox-gap);
+    font-family: var(--hybrid-checkbox-font-family);
+  }
 
-    @media (prefers-color-scheme: dark) {
-      .label {
-         color: var(--hy-checkbox-label-color, #fff);
-      }
-      path{
-        fill: #191515;
-      }
-      .checkbox {
-        --hy-checkbox-checked-color : #00ccff;
-      }
+  :host(:not([checked]):not([indeterminate]):not([disabled])) input {
+    background-color: var(--hybrid-checkbox-empty-background-color);
+    border: var(--hybrid-checkbox-empty-border);
+  }
 
+  :host(:not([disabled])[checked]) input,
+  :host(:not([disabled])[indeterminate]) input {
+    background-color: var(--hybrid-checkbox-filled-background-color);
+  }
+  :host([disabled]) {
+    color: var(--hybrid-checkbox-disabled-text-color);
+  }
+  :host([disabled]) input {
+    background-color: var(--hybrid-checkbox-disabled-background-color);
+    cursor: not-allowed;
+  }
+  input {
+    appearance: none;
+    width: var(--hybrid-checkbox-medium-width);
+    height: var(--hybrid-checkbox-medium-height);
+    cursor: pointer;
+    position: relative;
+    border-radius: var(--hybrid-checkbox-border-radius);
+  }
+  input:after {
+    color: var(--hybrid-checkbox-symbol-color);
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -51%);
+  }
+  input:focus {
+    border: var(--hybrid-checkbox-focus-border);
+    outline: var(--hybrid-checkbox-focus-outline);
+  }
+  :host([size='large']) input {
+    width: var(--hybrid-checkbox-large-width);
+    height: var(--hybrid-checkbox-large-height);
+  }
+  :host([size='small']) input {
+    width: var(--hybrid-checkbox-small-width);
+    height: var(--hybrid-checkbox-small-height);
+  }
+
+  :host([indeterminate][size='medium']) input:after {
+    font-size: var(--hybrid-checkbox-medium-indeterminate-size);
+  }
+  :host([indeterminate][size='large']) input:after {
+    font-size: var(--hybrid-checkbox-large-indeterminate-size);
+  }
+
+  :host([indeterminate][size='small']) input:after {
+    font-size: var(--hybrid-checkbox-small-indeterminate-size);
+  }
+
+  :host([checked]) input:after {
+    border: solid;
+    border-width: 0 2px 2px 0;
+    transform: translate(-50%, -51%) rotate(45deg);
+    content: '';
+  }
+  :host([checked][size='small']) input:after {
+    width: var(--hybrid-checkbox-small-checked-width);
+    height: var(--hybrid-checkbox-small-checked-height);
+  }
+  :host([checked][size='large']) input:after {
+    width: var(--hybrid-checkbox-large-checked-width);
+    height: var(--hybrid-checkbox-large-checked-height);
+  }
+  :host([checked][size='medium']) input:after {
+    width: var(--hybrid-checkbox-medium-checked-width);
+    height: var(--hybrid-checkbox-medium-checked-height);
+  }
+
+  :host([indeterminate]:not([checked])) input:after {
+    content: '-';
+  }
+  :host(:not([checked]):not([indeterminate])) input:after {
+    content: '';
   }
 `;
+export const styles = [checkBoxStyles, styleVariables];
---
+++ components/ui/nuraly-ui/src/components/checkbox/checkbox.variables.ts
@@ -0,0 +1,47 @@
+import {css} from 'lit';
+
+export const styleVariables = css`
+  :host {
+    --hybrid-checkbox-filled-background-color: #161616;
+    --hybrid-checkbox-empty-background-color: #ffffff;
+    --hybrid-checkbox-disabled-background-color: #c6c6c6;
+    --hybrid-checkbox-disabled-text-color: #c6c6c6;
+    --hybrid-checkbox-empty-border: 1px solid #161616;
+    --hybrid-checkbox-symbol-color: #ffffff;
+    --hybrid-checkbox-focus-border: 2px solid #ffffff;
+    --hybrid-checkbox-focus-outline: 2px solid #0f62fe;
+    --hybrid-checkbox-font-family: IBM Plex Sans;
+    --hybrid-checkbox-border-radius: 1px;
+    --hybrid-checkbox-gap: 5px;
+
+    --hybrid-checkbox-medium-width: 20px;
+    --hybrid-checkbox-medium-height: 20px;
+    --hybrid-checkbox-small-width: 15px;
+    --hybrid-checkbox-small-height: 15px;
+    --hybrid-checkbox-large-width: 25px;
+    --hybrid-checkbox-large-height: 25px;
+
+    --hybrid-checkbox-small-indeterminate-size: 10px;
+    --hybrid-checkbox-large-indeterminate-size: 18px;
+    --hybrid-checkbox-medium-indeterminate-size: 13px;
+
+    --hybrid-checkbox-small-checked-width: 2px;
+    --hybrid-checkbox-small-checked-height: 7px;
+    --hybrid-checkbox-medium-checked-width: 3px;
+    --hybrid-checkbox-medium-checked-height: 9px;
+    --hybrid-checkbox-large-checked-width: 4px;
+    --hybrid-checkbox-large-checked-height: 10px;
+  }
+  @media (prefers-color-scheme: dark) {
+    :host {
+      --hybrid-checkbox-empty-border: 1px solid #ffffff;
+      --hybrid-checkbox-empty-background-color: #161616;
+      --hybrid-checkbox-filled-background-color: #ffffff;
+      --hybrid-checkbox-symbol-color: #161616;
+      --hybrid-checkbox-focus-outline: 2px solid #ffffff;
+      --hybrid-checkbox-focus-border: 2px solid #161616;
+      --hybrid-checkbox-disabled-background-color: #6f6f6f;
+      --hybrid-checkbox-disabled-text-color: #6f6f6f;
+    }
+  }
+`;
