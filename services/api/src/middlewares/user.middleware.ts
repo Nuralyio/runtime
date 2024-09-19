761783bb4ab59f3a02cf441fa6a9d12ce00fd651
Aymen <labidi@aymen.co>
2024-09-19 15:37:43 +0100

    Revert "feat: Fix authentication middleware bug in user.middleware.ts"
    This reverts commit 7c4dc349aaef7f2cf1a25b1b11dc1f4cca011090.

--- services/api/src/middlewares/user.middleware.ts
+++ services/api/src/middlewares/user.middleware.ts
@@ -16,21 +16,19 @@ const handleError = (res: Response, status: number, logMessage: string, response
 };
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const headerValue = req.header('X-USER') ?? undefined;
+    const headerValue = req.header('X-USER');
 
     if (!headerValue) {
-        // return handleError(res, 401, "No authorization header provided. Please include a Bearer token in the Authorization header.", 'No authorization header provided. Please include a Bearer token in the Authorization header.');
+        return handleError(res, 401, "No authorization header provided. Please include a Bearer token in the Authorization header.", 'No authorization header provided. Please include a Bearer token in the Authorization header.');
     }
 
     try {
-        const user: User = JSON.parse(headerValue as string);
+        const user: User = JSON.parse(headerValue);
         (req as any).user = user;
         next();
     } catch (error) {
-        // return handleError(res, 400, "Invalid user data in header. Please ensure the user data is correctly formatted.", 'Invalid user data in header. Please ensure the user data is correctly formatted.');
+        return handleError(res, 400, "Invalid user data in header. Please ensure the user data is correctly formatted.", 'Invalid user data in header. Please ensure the user data is correctly formatted.');
     }
-    next();
-
 };
 
 export default authMiddleware;
