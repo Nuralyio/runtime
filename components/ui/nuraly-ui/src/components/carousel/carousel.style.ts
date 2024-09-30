3a90d0e6cf65aa9db95e6261d136597b8188a417
mdevuser1 <labidi@aymen.co>
2024-09-30 11:37:46 +0100

    Add carousel component

---
+++ components/ui/nuraly-ui/src/components/carousel/carousel.style.ts
@@ -0,0 +1,69 @@
+import {css} from 'lit';
+
+const carouselStyles = css`
+  :host {
+    position: relative;
+  }
+
+  .carousel {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  ::slotted(.carousel-item-hidden) {
+    position: absolute;
+    opacity: 0;
+  }
+  ::slotted(:not(.carousel-item-hidden)) {
+    opacity: 1;
+    transition: opacity 0.5s ease;
+  }
+
+  .controls {
+    position: absolute;
+    width: 100%;
+    top: 50%;
+    display: flex;
+    justify-content: space-between;
+  }
+  .button-control {
+    --hybrid-button-ghost-text-color: grey;
+    --hybrid-button-ghost-active-border-color: transparent;
+    --hybrid-button-ghost-hover-background-color: transparent;
+    --hybrid-button-ghost-hover-border-color: transparent;
+  }
+
+  .dot {
+    height: 8px;
+    width: 8px;
+    margin: 0 5px;
+    background-color: var(--carousel-dot-background-color);
+    display: inline-block;
+    cursor: pointer;
+    opacity: 0.4;
+  }
+
+  .dot.active {
+    background-color: var(--carousel-dot-active-background-color);
+    opacity: 0.9;
+  }
+
+  :host {
+    --carousel-dot-background-color: #bebebe;
+    --carousel-dot-active-background-color: gray;
+  }
+  @media (prefers-color-scheme: dark) {
+    :host {
+      --carousel-dot-background-color: gray;
+      --carousel-dot-active-background-color: lightgray;
+      .button-control {
+        --hybrid-button-ghost-text-color: #ffffff;
+      }
+    }
+  }
+`;
+
+export const styles = carouselStyles;
