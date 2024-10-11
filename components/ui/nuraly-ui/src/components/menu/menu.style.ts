d9b4fcacf3145dd382a9aa9e9d4d892d38f2122f
Aymen <labidi@aymen.co>
2024-10-11 06:48:43 -0700

    Enhance menu styling

--- components/ui/nuraly-ui/src/components/menu/menu.style.ts
+++ components/ui/nuraly-ui/src/components/menu/menu.style.ts
@@ -5,23 +5,27 @@ const menuStyle = css`
     width: var(--hybrid-menu-width);
     padding: var(--hybrid-menu-padding);
     border: var(--hybrid-menu-border);
+    font-size: var(--hybrid-menu-font-size);
+    font-family: var(--hybrid-menu-font-family)
   }
   :host {
+    --hybrid-menu-font-size:13px;
+    --hybrid-menu-font-family:serif;
     --hybrid-menu-padding: 2px;
     --hybrid-menu-width: 45%;
     --hybrid-menu-border: 1px solid #f4f4f4;
-    --hybrid-menu-link-color: #525252;
+    --hybrid-menu-link-color: #000000;
     --hybrid-menu-link-icon-color: #525252;
     --hybrid-menu-disabled-link-color: #c6c6c6;
     --hybrid-menu-focus-border: 1px solid #0f62fe;
-    --hybrid-menu-focus-color: #525252;
-    --hybrid-menu-active-color: #161616;
+    --hybrid-menu-focus-color: #000000;
+    --hybrid-menu-active-color: #000000;
     --hybrid-menu-active-background-color: #c6c6c6;
     --hybrid-menu-hover-link-background-color: #f4f4f4;
-    --hybrid-menu-hover-link-color: #161616;
+    --hybrid-menu-hover-link-color: #000000;
     --hybrid-menu-selected-link-background-color: #e0e0e0;
     --hybrid-menu-selected-link-border: 3px solid #0f62fe;
-    --hybrid-menu-selected-color: #161616;
+    --hybrid-menu-selected-color: #000000;
     --hybrid-menu-link-background-color: transparent;
     --hybrid-menu-link-padding-y: 8px;
     --hybrid-menu-link-empty-icon-padding-left: 39px;
@@ -29,15 +33,16 @@ const menuStyle = css`
     --hybrid-menu-link-icon-padding-left: 10px;
     --hybrid-menu-link-icon-right-padding-left: 39px;
     --hybrid-menu-link-icon-only-width: 42px;
-    --hybrid-sub-menu-color: #525252;
+    --hybrid-sub-menu-color: #000000;
     --hybrid-sub-menu-disabled-color: #c6c6c6;
     --hybrid-sub-menu-focus-color: #525252;
     --hybrid-sub-menu-focus-border: 1px solid #0f62fe;
-    --hybrid-sub-menu-hover-background-color: #e5e5e5;
+    --hybrid-sub-menu-hover-background-color: #f4f4f4;
     --hybrid-sub-menu-hover-color: #161616;
     --hybrid-sub-menu-active-background-color: #c6c6c6;
-    --hybrid-sub-menu-active-color: #161616;
-    --hybrid-sub-menu-highlighted-color: #0f62fe;
+    --hybrid-sub-menu-active-color: #000000;
+    --hybrid-sub-menu-highlighted-color: #000000;
+    --hybrid-sub-menu-highlighted-background-color: #e2e2e2;
     --hybrid-sub-menu-text-icon-color: #525252;
     --hybrid-sub-menu-action-icon-color:#525252;
     --hybrid-sub-menu-status-icon-color:#525252;
@@ -52,25 +57,27 @@ const menuStyle = css`
   @media (prefers-color-scheme: dark) {
     :host {
       --hybrid-menu-border: 1px solid #ffffff;
-      --hybrid-menu-link-color:#c6c6c6;
+      --hybrid-menu-link-color:#ffffff;
       --hybrid-menu-focus-border: 1px solid #ffffff;
-      --hybrid-menu-focus-color: #525252;
-      --hybrid-menu-hover-link-background-color: #f4f4f4;
-      --hybrid-menu-hover-link-color: #161616;
-      --hybrid-menu-active-color: #161616;
-      --hybrid-menu-active-background-color: #c6c6c6;
+      --hybrid-menu-focus-color: #ffffff;
+      --hybrid-menu-hover-link-background-color: #333333;
+      --hybrid-menu-hover-link-color: #ffffff;
+      --hybrid-menu-active-color: #ffffff;
+      --hybrid-menu-active-background-color: #242424;
       --hybrid-menu-selected-link-border: 3px solid #ffffff;
-      --hybrid-menu-selected-link-background-color: #e0e0e0;
+      --hybrid-menu-selected-link-background-color: #3a3a3a;
+      --hybrid-menu-selected-color:#ffffff;
       --hybrid-menu-disabled-link-color: #525252;
-      --hybrid-sub-menu-color: #c6c6c6;
+      --hybrid-sub-menu-color: #ffffff;
       --hybrid-sub-menu-disabled-color: #525252;
       --hybrid-sub-menu-focus-border: 1px solid #ffffff;
       --hybrid-sub-menu-focus-color: #525252;
-      --hybrid-sub-menu-hover-background-color: #f4f4f4;
-      --hybrid-sub-menu-hover-color: #161616;
-      --hybrid-sub-menu-active-background-color: #c6c6c6;
-      --hybrid-sub-menu-active-color: #161616;
+      --hybrid-sub-menu-hover-background-color: #333333;
+      --hybrid-sub-menu-hover-color: #ffffff;
+      --hybrid-sub-menu-active-background-color: #242424;
+      --hybrid-sub-menu-active-color: #ffffff;
       --hybrid-sub-menu-highlighted-color: #ffffff;
+      --hybrid-sub-menu-highlighted-background-color: #3a3a3a;
       --hybrid-sub-menu-text-icon-color: #525252;
       --hybrid-sub-menu-toggle-icon-color: #525252;
     }
