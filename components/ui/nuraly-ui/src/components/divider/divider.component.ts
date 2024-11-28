684779970662a2454c33010fce7617330fa5d400
Aymen <labidi@aymen.co>
2024-11-28 14:19:33 +0100

    feat(divider): add hy-divider component with customizable styles and properties

---
+++ components/ui/nuraly-ui/src/components/divider/divider.component.ts
@@ -0,0 +1,42 @@
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import styles from "./divider.style.js";
+
+@customElement('hy-divider')
+export class HyDivider extends LitElement {
+  static override styles = styles;
+
+  @property({type: String})
+  color = '';
+
+  @property({type: String})
+  darkColor = '';
+
+  @property({type: String})
+  lightColor = '';
+
+  @property({type: String})
+  orientation: 'horizontal' | 'vertical' = 'horizontal';
+
+  @property({type: Number})
+  thickness = 1;
+
+  override willUpdate(changedProperties: {has: (arg0: string) => any}) {
+    if (changedProperties.has('color')) {
+      this.style.setProperty('--hy-divider-color', this.color);
+    }
+    if (changedProperties.has('darkColor')) {
+      this.style.setProperty('--hy-divider-local-dark-color', this.darkColor);
+    }
+    if (changedProperties.has('lightColor')) {
+      this.style.setProperty('--hy-divider-local-light-color', this.lightColor);
+    }
+    if (changedProperties.has('thickness')) {
+      this.style.setProperty('--hy-divider-thickness', `${this.thickness}px`);
+    }
+  }
+
+  override render() {
+    return html` <div class="divider ${this.orientation}"></div> `;
+  }
+}
