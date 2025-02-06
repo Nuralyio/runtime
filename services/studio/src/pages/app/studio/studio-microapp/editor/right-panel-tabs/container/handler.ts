0c4c00182202b5ecbd5adbd531010c1739b43d3f
Aymen <labidi@aymen.co>
2025-02-06 12:08:38 +0100

    feat: add onClick event handler to StudioContainerHandler for improved interaction

--- services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/container/handler.ts
+++ services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/container/handler.ts
@@ -3,5 +3,8 @@ import { createHandlersFromEvents } from "../../utils/handler-generator.ts";
 
 export const StudioContainerHandler = createHandlersFromEvents
 ([
-
+    {
+        name: "onClick",
+        label: "onClick"
+      },
 ], "studio_container_handler");
