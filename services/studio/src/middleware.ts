c2312f2b16a7b6b83ed51365c6c59fd0da4baeaf
Aymen <labidi@aymen.co>
2025-04-08 18:46:09 +0100

    feat: enhance MicroApp component initialization with detailed logging and streamline page handling

--- services/studio/src/middleware.ts
+++ services/studio/src/middleware.ts
@@ -6,7 +6,7 @@ import { defineMiddleware } from "astro:middleware";
 
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
-  console.debug("re -initing stores");
+  // console.debug("re -initing stores");
   $applications.set([]);
   $providers.set([]);
   $pages.set({});
