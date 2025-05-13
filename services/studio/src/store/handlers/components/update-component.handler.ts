45c86001c4e20d146379f1db0d3a3dbb532db7a2
Aymen <labidi@aymen.co>
2025-05-13 09:57:36 +0100

    refactor: Update updateComponentHandler to use a modified component object for API requests

--- services/studio/src/store/handlers/components/update-component.handler.ts
+++ services/studio/src/store/handlers/components/update-component.handler.ts
@@ -1,14 +1,16 @@
 import { FRONT_API_URLS } from "$store/handlers/api-urls";
-import type { ComponentElement } from "../../component/interface";
 
-export const updateComponentHandler = (component: ComponentElement, application_id) => {
-  fetch(`${FRONT_API_URLS.COMPONENTS}/${component.uuid}`, {
+export const updateComponentHandler = (component: any, application_id) => {
+  const ucomponent = { ...component };
+  delete ucomponent.parent
+  delete ucomponent.children
+  fetch(`${FRONT_API_URLS.COMPONENTS}/${ucomponent.uuid}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      component: { ...component, application_id }
+      component: { ...ucomponent, application_id }
     })
   }).then(res => res.json())
     .catch((err) => {
