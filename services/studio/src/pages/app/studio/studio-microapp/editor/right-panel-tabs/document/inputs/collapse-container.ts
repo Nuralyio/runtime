1b4fc7fd747ba878a324ec63ec211ae93615136d
Aymen <labidi@aymen.co>
2025-05-17 16:17:45 +0100

    fix: Format import statements and add 'size_collapse_container' to dynamic container in both document and video collapse containers

--- services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/document/inputs/collapse-container.ts
+++ services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/document/inputs/collapse-container.ts
@@ -1,8 +1,9 @@
 
-import {generateDynamicContainer} from "../../../utils/input-collapse-container-generator.ts";
+import { generateDynamicContainer } from "../../../utils/input-collapse-container-generator.ts";
 
 export const StudioCollapseContainer = generateDynamicContainer("document_collapse_container", [
   "document_document_previewable_text_block",
   "document_src_text_block",
+  "size_collapse_container",
   "display_block"
 ]);
