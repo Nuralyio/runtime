471d919d0c1c2ff05082725a9418b32b9d2a75e9
Aymen <labidi@aymen.co>
2025-05-17 16:52:00 +0100

    fix: Remove unnecessary blank line in collapse-container.ts for cleaner code

--- services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/document/inputs/collapse-container.ts
+++ services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/document/inputs/collapse-container.ts
@@ -1,6 +1,7 @@
 
 import { generateDynamicContainer } from "../../../utils/input-collapse-container-generator.ts";
 
+
 export const StudioCollapseContainer = generateDynamicContainer("document_collapse_container", [
   "document_document_previewable_text_block",
   "document_src_text_block",
