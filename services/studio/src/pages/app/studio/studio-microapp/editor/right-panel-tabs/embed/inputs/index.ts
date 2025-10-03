4d54e9e26e31d909383a014c41578b3672c1173d
Aymen <labidi@aymen.co>
2025-10-04 00:06:58 +0100

    feat(embed): add embed component with configuration, handlers, theme, and integrate into the main component structure

--- services/studio/src/pages/app/studio/studio-microapp/editor/right-panel-tabs/embed/inputs/index.ts
+++
@@ -1,7 +0,0 @@
-import { StudioEmbedCollapseContainer } from "./collapse-container";
-import { EmbedURLInput } from "./url";
-
-export const StudioEmbedInputs = [
-     ...StudioEmbedCollapseContainer,
-     ...EmbedURLInput
-]
--- services/studio/src/pages/app/studio/studio-microapp/studio-entrypoint.ts
+++ services/studio/src/pages/app/studio/studio-microapp/studio-entrypoint.ts
@@ -42,7 +42,7 @@ import RightPanelTabs from "./editor/right-panel-tabs/right_panel_tabs.ts";
 import RightPanelFunctionTab from "./editor/right-panel-tabs/right_panel_function_tabs.ts";
 
 
-import { StudioTextInput, StudioTextLabel, StudioDatepicker, StudioSelect, StudioButton, StudioCheckbox, StudioCode, StudioCollection, StudioContainer, StudioDocument, StudioDropdown } from "./editor/right-panel-tabs/index.ts";
+import { StudioTextInput, StudioTextLabel, StudioDatepicker, StudioSelect, StudioButton, StudioCheckbox, StudioCode, StudioCollection, StudioContainer, StudioDocument, StudioDropdown, StudioEmbed } from "./editor/right-panel-tabs/index.ts";
 import { StudioIcon } from "./editor/right-panel-tabs/icon";
 import { StudioImage } from "./editor/right-panel-tabs/image";
 import { StudioTable } from "./editor/right-panel-tabs/table";
@@ -54,7 +54,6 @@ import { PageThemeStudio } from "./page-blocks/themes.ts";
 import { StudioMenu } from "./editor/right-panel-tabs/menu/index.ts";
 import { StudioRichText } from "./editor/right-panel-tabs/Richtext/index.ts";
 import { StudioRichTextEditor } from "./editor/right-panel-tabs/RichtextEditor/index.ts";
-import { StudioEmbed } from "./editor/right-panel-tabs/embed/index.ts";
 import { StudioLink } from "./editor/right-panel-tabs/link/index.ts";
 import { StudioFileUpload } from "./editor/right-panel-tabs/file-upload/index.ts";
 import { StudioVideo } from "./editor/right-panel-tabs/video";
