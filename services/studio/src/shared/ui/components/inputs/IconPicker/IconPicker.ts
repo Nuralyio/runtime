6d0de1a4b180b4c2e4c29ad165712891705c6a1a
Aymen <labidi@aymen.co>
2025-11-11 14:14:27 +0100

    fix(IconPicker): Update icon picker input type and improve styles for better usability

--- services/studio/src/shared/ui/components/inputs/IconPicker/IconPicker.ts
+++ services/studio/src/shared/ui/components/inputs/IconPicker/IconPicker.ts
@@ -1,132 +1,61 @@
 import type { ComponentElement } from "@shared/redux/store/component/component.interface.ts";
 import { html } from "lit";
-import { customElement, property, state } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
 import { BaseElementBlock } from "@shared/ui/components/base/BaseElement.ts";
 import { styles } from "./IconPicker.style.ts";
-import { executeHandler } from "@features/runtime/state/runtime-context.ts";
-import { getNestedAttribute } from "@shared/utils/object.utils.ts";
 import { EMPTY_STRING } from "@shared/utils/constants.ts";
-import * as solidIcons from "@fortawesome/free-solid-svg-icons";
-import "@lit-labs/virtualizer";
-import { grid } from "@lit-labs/virtualizer/layouts/grid.js";
 import { styleMap } from "lit/directives/style-map.js";
-import { ButtonTheme } from "@features/studio/core/utils/common-editor-theme";
+import { ref } from "lit/directives/ref.js";
+import "@nuralyui/iconpicker";
 
 @customElement("icon-picker-block")
 export class IconPicker extends BaseElementBlock {
   static override styles = styles;
-  icons = Array.from(
-    new Set(
-      [...Object.keys(solidIcons)]
-        .filter((key) => key.startsWith("fa"))
-        .map((key) => solidIcons[key].iconName)
-        .filter((iconName) => iconName)
-    )
-  );
-  @state()
-  filteredIcons = this.icons;
-  @state()
-  selectedIcon = EMPTY_STRING;
-  @state()
-  dropdownOpen = false;
-  @property()
+
+  @property({ type: Object })
   component: ComponentElement;
 
-  constructor() {
-    super();
-    document.addEventListener("click", this.onClickOutside);
+  override async connectedCallback() {
+    await super.connectedCallback();
+    this.registerCallback("value", () => {});
   }
 
-  handleIconSelect(icon: string) {
-    this.selectedIcon = icon === this.selectedIcon ? EMPTY_STRING : icon;
-    this.dispatchEvent(new CustomEvent("icon-selected", { detail: icon }));
-
-    if (this.component.event?.iconChanged) {
-      executeHandler(this.component, getNestedAttribute(this.component, `event.iconChanged`), {
-        value: this.selectedIcon
-      });
-    }
-    this.dropdownOpen = false;
-    this.requestUpdate();
-  }
-
-  handleIconChange = (e: CustomEvent) => {
-    const searchString = e.detail.value;
-    this.filteredIcons = searchString
-      ? this.icons.filter((icon) => icon.includes(searchString))
-      : this.icons;
-  };
-
-  toggleDropdown = () => {
-    this.dropdownOpen = !this.dropdownOpen;
-  };
-
-  override disconnectedCallback(): void {
-    super.disconnectedCallback();
-    document.removeEventListener("click", this.onClickOutside);
-  }
-
-  override render() {
-    this.selectedIcon = this.inputHandlersValue.value ?? EMPTY_STRING;
-    const isDisabled = this.inputHandlersValue?.disable || false;
-    
-    const dropdownContent = html`
-      <style>
-        .icon-item .selected {
-          border: red;
-        }
-      </style>
-      <div class="dropdown-icon">
-        <div class="search-container">
-          <nr-input
-            .autocomplete="off"
-            placeholder=${this.inputHandlersValue.placeholder || "Search icons..."}
-            @nr-input=${this.handleIconChange}
-          ></nr-input>
-        </div>
-        <div style="height: 200px; overflow: auto; display: flex; background: var(--nuraly-input-background-color)" class="hello">
-          <lit-virtualizer
-            .items=${this.filteredIcons}
-            .layout=${grid({ itemSize: "30px" })}
-            .renderItem=${(icon: string) => html`
-              <div
-                class="icon-item ${icon === this.selectedIcon ? "selected" : EMPTY_STRING}"
-                @click=${() => this.handleIconSelect(icon)}
-              >
-                <nr-icon .name=${icon}></nr-icon>
-              </div>
-            `}
-          ></lit-virtualizer>
-        </div>
-      </div>
-    `;
+  renderComponent() {
+    const iconPickerStyles = this.component?.style || {};
+    const size = (iconPickerStyles.size as 'small' | 'medium' | 'large') || 'medium';
 
     return html`
-      <nr-dropdown
-        style="--nuraly-dropdown-width: 215px;"
-        .open=${this.dropdownOpen}
-        .items=${[{ content: dropdownContent }]}
-      >
-        <nr-button
-          slot="trigger"
-          .disabled=${isDisabled}
-          @click=${this.toggleDropdown}
-          style=${styleMap({
-            ...ButtonTheme,
-            "--nuraly-button-width": isDisabled ? "auto" : "35px"
-          })}
-        >
-          ${isDisabled 
-            ? html`Dynamic` 
-            : html`<nr-icon class="icon-preview" .name=${this.selectedIcon}></nr-icon>`
-          }
-        </nr-button>
-      </nr-dropdown>
+      <nr-icon-picker
+        ${ref(this.inputRef)}
+        style=${styleMap({
+          ...this.getStyles(),
+        })}
+        .value=${this.inputHandlersValue.value ?? EMPTY_STRING}
+        .disabled=${this.inputHandlersValue?.state === "disabled"}
+        .readonly=${this.inputHandlersValue?.readonly || false}
+        .size=${size}
+        .placement=${this.inputHandlersValue?.placement || 'auto'}
+        .trigger=${this.inputHandlersValue?.trigger || 'manual'}
+        .placeholder=${this.inputHandlersValue?.placeholder || 'Select icon'}
+        .showSearch=${this.inputHandlersValue?.showSearch !== false}
+        .showClear=${this.inputHandlersValue?.showClear !== false}
+        .maxVisible=${this.inputHandlersValue?.maxVisible || 500}
+        @nr-icon-picker-change=${(e: CustomEvent) => {
+          this.executeEvent('onChange', e, { value: e.detail.value, icon: e.detail.icon });
+        }}
+        @nr-icon-picker-open=${(e: CustomEvent) => {
+          this.executeEvent('onOpen', e);
+        }}
+        @nr-icon-picker-close=${(e: CustomEvent) => {
+          this.executeEvent('onClose', e);
+        }}
+        @nr-icon-picker-search=${(e: CustomEvent) => {
+          this.executeEvent('onSearch', e, { query: e.detail.query });
+        }}
+        @nr-icon-picker-clear=${(e: CustomEvent) => {
+          this.executeEvent('onClear', e);
+        }}
+      ></nr-icon-picker>
     `;
   }
-
-  private onClickOutside = (event: Event) => {
-    const outsideClick = !event.composedPath().includes(this);
-    if (outsideClick) this.dropdownOpen = false;
-  };
 }
