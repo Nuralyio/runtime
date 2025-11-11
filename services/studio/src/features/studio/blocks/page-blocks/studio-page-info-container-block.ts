44a9e12970c8f0d7496e00ee57b260beed01f01a
Aymen <labidi@aymen.co>
2025-11-11 11:51:00 +0100

    feat(studio-blocks): Add page info container and default checkbox blocks; update right panel tabs and entry point

---
+++ services/studio/src/features/studio/blocks/page-blocks/studio-page-info-container-block.ts
@@ -0,0 +1,21 @@
+import { ComponentType } from "@shared/redux/store/component/component.interface.ts";
+import { COMMON_ATTRIBUTES } from "../../core/helpers/common_attributes.ts";
+
+export default [
+  {
+    uuid: "page_info_container_block",
+    application_id: "1",
+    name: "page info container block",
+    component_type: ComponentType.Container,
+    ...COMMON_ATTRIBUTES,
+    style: {
+      display: "flex",
+      "flex-direction": "column",
+      gap: "8px",
+      width: "100%",
+      padding: "8px",
+      "margin-bottom" : "8px"
+    },
+    childrenIds: ["page_name_block", "page_url_block", "description_block", "page_default_checkbox_block"]
+  }
+];
