b75e936aea545901de8fb8106d1a4c29fbf0e694
Aymen <labidi@aymen.co>
2025-12-13 08:45:32 +0100

    fix: update Select component properties and handlers, enhance configuration and theme structure

--- components/ui/components/inputs/Select/Select.ts
+++ components/ui/components/inputs/Select/Select.ts
@@ -3,8 +3,6 @@ import { customElement, property } from "lit/decorators.js";
 import { styleMap } from "lit/directives/style-map.js";
 import { type ComponentElement } from '../../../../../redux/store/component/component.interface.ts';
 import { BaseElementBlock } from "../../base/BaseElement.ts";
-import { executeHandler } from '../../../../../state/runtime-context.ts';
-import { getNestedAttribute } from '../../../../../utils/object.utils.ts';
 import { EMPTY_STRING } from '../../../../../utils/constants.ts';
 import { ref } from "lit/directives/ref.js";
 import "@nuralyui/select";
@@ -27,35 +25,51 @@ export class SelectBlock extends BaseElementBlock {
     });
   }
 
-  handleValueChange = (customEvent: CustomEvent) => {
-    console.log('[SelectBlock] nr-change event:', customEvent.detail);
-    if (this.component.event.onSelect) {
-      const optionValue = customEvent.detail.value ?? EMPTY_STRING;
-      executeHandler(this.component, getNestedAttribute(this.component, `event.onSelect`), {
-        value: optionValue
-      });
-    }
-  };
-
 override  renderComponent() {
 
     return html`
-      <nr-select  
+      <nr-select
             ${ref(this.inputRef)}
-            style=${styleMap({...this.getStyles(), 
-            "--nuraly-select-width" : this.getStyles()['--nuraly-select-width']  ?? this.getStyles().width    
-          })}
+            style=${styleMap({
+              ...this.getStyles(),
+              "--nuraly-select-width": (this.getStyles() as any)['--nuraly-select-width'] ?? this.getStyles().width
+            })}
                  ?multiple=${this.inputHandlersValue?.selectionMode === "multiple"}
                  ?searchable=${this.inputHandlersValue?.searchable ?? false}
+                 ?clearable=${this.inputHandlersValue?.clearable ?? false}
+                 ?required=${this.inputHandlersValue?.required ?? false}
+                 ?block=${this.inputHandlersValue?.block ?? false}
                  .options=${this.inputHandlersValue?.options  ?? []}
                  .value=${this.inputHandlersValue?.value ?? EMPTY_STRING}
-                 .placeholder=${this.inputHandlersValue.placeholder || "Select an option"}
-                 search-placeholder=${this.inputHandlersValue?.searchPlaceholder || "Search options..."}
+                 .placeholder=${this.inputHandlersValue?.placeholder || "Select an option"}
+                 .searchPlaceholder=${this.inputHandlersValue?.searchPlaceholder || "Search options..."}
                  .status=${this.inputHandlersValue?.state ?? nothing}
                  .size=${this.inputHandlersValue?.size ?? nothing}
-                 .disabled=${(this.inputHandlersValue.state == "disabled")}
-                 .type=${this.inputHandlersValue.type == "inline" ? "inline" : nothing}
-                 @nr-change=${this.handleValueChange}
+                 .disabled=${this.inputHandlersValue?.disabled ?? (this.inputHandlersValue.state == "disabled")}
+                 .type=${this.inputHandlersValue?.type || "default"}
+                 @nr-change=${(e: CustomEvent) => {
+                   this.executeEvent('onChange', e, {
+                     value: e.detail.value ?? EMPTY_STRING
+                   });
+                 }}
+                 @nr-focus=${(e: CustomEvent) => {
+                   this.executeEvent('onFocus', e);
+                 }}
+                 @nr-blur=${(e: CustomEvent) => {
+                   this.executeEvent('onBlur', e);
+                 }}
+                 @nr-dropdown-open=${(e: CustomEvent) => {
+                   this.executeEvent('onDropdownOpen', e);
+                 }}
+                 @nr-dropdown-close=${(e: CustomEvent) => {
+                   this.executeEvent('onDropdownClose', e);
+                 }}
+                 @nr-validation=${(e: CustomEvent) => {
+                   this.executeEvent('onValidation', e, {
+                     isValid: e.detail.isValid,
+                     message: e.detail.message
+                   });
+                 }}
       >
         <span slot="label">${this.inputHandlersValue.label ?? nothing}</span>
         <span slot="helper-text">${this.inputHandlersValue.helper ?? nothing}</span>
