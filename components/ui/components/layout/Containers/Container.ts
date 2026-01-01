20804e51db755d20c54894791769cc59304b8e83
Aymen <labidi@aymen.co>
2026-01-01 18:29:42 +0100

    feat: Refactor Container component styles and structure for improved layout handling

--- components/ui/components/layout/Containers/Container.ts
+++ components/ui/components/layout/Containers/Container.ts
@@ -1,33 +1,30 @@
-import { type ComponentElement, type DraggingComponentInfo } from '../../../../../redux/store/component/component.interface.ts';
+import { type ComponentElement } from '../../../../../redux/store/component/component.interface.ts';
 import { $components } from '../../../../../redux/store/component/store.ts';
 import { html, nothing } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
-import { classMap } from "lit/directives/class-map.js";
-import { styleMap } from "lit/directives/style-map.js";
 import { renderComponent } from '../../../../../utils/render-util';
 import { createRef, type Ref, ref } from "lit/directives/ref.js";
 import styles from "./Container.style.ts";
 import { BaseElementBlock } from "../../base/BaseElement.ts";
 import { setCurrentComponentIdAction } from '../../../../../redux/actions/component/setCurrentComponentIdAction.ts';
 import { setContextMenuEvent } from '../../../../../redux/actions/page/setContextMenuEvent.ts';
-
+import "@nuralyui/container";
 @customElement("vertical-container-block")
 export class VerticalContainer extends BaseElementBlock {
   static styles = styles;
 
-  @property({ type: Object }) component: ComponentElement;
+  @property({ type: Object }) component!: ComponentElement;
   @property({ type: Object }) item: any;
-  @property({ type: Object }) draggingComponentInfo: DraggingComponentInfo;
   @property({ type: Boolean }) isViewMode = false;
 
   @state() dragOverSituation = false;
-  @state() selectedComponent: ComponentElement;
-  @state() hoveredComponent: ComponentElement;
+  @state() selectedComponent!: ComponentElement;
+  @state() hoveredComponent!: ComponentElement;
   @state() wrapperStyle: any = {};
   @state() containerRef: Ref<HTMLInputElement> = createRef();
   @state() childrenComponents: ComponentElement[] = [];
 
-  isDragging: boolean;
+  isDragging!: boolean;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -47,25 +44,82 @@ export class VerticalContainer extends BaseElementBlock {
     }) ?? [];
   }
 
+  /**
+   * Get container layout type based on input
+   */
+  private getLayout(): 'fluid' | 'boxed' | 'fixed' {
+    if (this.inputHandlersValue.layout === "boxed") return "boxed";
+    if (this.inputHandlersValue.layout === "fixed") return "fixed";
+    return "fluid";
+  }
+
+  /**
+   * Get container direction based on input or styles
+   */
+  private getDirection(): 'row' | 'column' {
+    // Check input direction first
+    if (this.inputHandlersValue.direction === "horizontal") return "row";
+    if (this.inputHandlersValue.direction === "vertical") return "column";
+    // Fallback: if display:flex is set in styles, default to row (browser default)
+    const styles = this.component?.style || {};
+    if (styles['display'] === 'flex' && !styles['flex-direction']) return "row";
+    if (styles['flex-direction'] === 'row') return "row";
+    // Default to column
+    return "column";
+  }
+
+  /**
+   * Get justify content value from input handlers
+   */
+  private getJustify(): '' | 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' {
+    const value = this.inputHandlersValue.justify;
+    if (value === 'flex-start' || value === 'flex-end' || value === 'center' ||
+        value === 'space-between' || value === 'space-around' || value === 'space-evenly') {
+      return value;
+    }
+    return '';
+  }
+
+  /**
+   * Get align items value from input handlers
+   */
+  private getAlign(): '' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch' {
+    const value = this.inputHandlersValue.align;
+    if (value === 'flex-start' || value === 'flex-end' || value === 'center' ||
+        value === 'baseline' || value === 'stretch') {
+      return value;
+    }
+    return '';
+  }
+
+  /**
+   * Get gap value from input handlers
+   */
+  private getGap(): number | string {
+    return this.inputHandlersValue.gap || 0;
+  }
+
   renderView() {
+    const componentStyles = this.getStyles();
+
     return html`
-      <div
-      id=${this.inputHandlersValue.id ?? nothing}
+      <nr-container
+        id=${this.inputHandlersValue.id ?? nothing}
         ${ref(this.inputRef)}
         data-component-uuid=${this.component?.uuid}
         data-component-name=${this.component?.name}
-        style=${styleMap({
-          "min-height": this.childrenComponents.length ? "auto" : "300px",
-        })}
-        class=${classMap({
-          container: true,
-          vertical: this.inputHandlersValue.direction === "vertical",
-          boxed: this.inputHandlersValue.layout === "boxed",
-          "drag-over": this.dragOverSituation,
-          [`drop-${this.component.uuid}`] : true,
-        })}
-        @mouseenter="${(e: Event) => this.executeEvent("onMouseEnter", {...e,unique : this.uniqueUUID})}"
-        @mouseleave="${(e: Event) => this.executeEvent("onMouseLeave", {...e,unique : this.uniqueUUID})}"
+        layout=${this.getLayout()}
+        direction=${this.getDirection()}
+        justify=${this.getJustify()}
+        align=${this.getAlign()}
+        .gap=${this.getGap()}
+        ?wrap=${this.inputHandlersValue.wrap}
+        width=${componentStyles.width || ''}
+        height=${componentStyles.height || ''}
+        min-height=${this.childrenComponents.length ? (componentStyles['min-height'] || '') : '300px'}
+        class="drop-${this.component.uuid}"
+        @mouseenter="${(e: Event) => this.executeEvent("onMouseEnter", e)}"
+        @mouseleave="${(e: Event) => this.executeEvent("onMouseLeave", e)}"
         @click="${(e: Event) => this.executeEvent("onClick", e)}"
       >
         ${this.childrenComponents.length
@@ -76,34 +130,35 @@ export class VerticalContainer extends BaseElementBlock {
                 <nr-label>No content</nr-label>
               </div>
             `}
-      </div>
+      </nr-container>
     `;
   }
 
   override renderComponent() {
+    const componentStyles = this.getStyles();
+
     return html`
       ${this.isViewMode
         ? this.renderView()
         : html`
-            <div
+            <nr-container
               ${ref(this.inputRef)}
               data-component-uuid=${this.component?.uuid}
               data-component-name=${this.component?.name}
-              ${ref(this.containerRef)}
+              layout=${this.getLayout()}
+              direction=${this.getDirection()}
+              justify=${this.getJustify()}
+              align=${this.getAlign()}
+              .gap=${this.getGap()}
+              ?wrap=${this.inputHandlersValue.wrap}
+              width=${componentStyles.width || ''}
+              height=${componentStyles.height || ''}
+              min-height=${this.childrenComponents.length ? (componentStyles['min-height'] || '') : '300px'}
+              class="drop-${this.component.uuid}"
               @click="${(e: Event) => {
                 setContextMenuEvent(null);
                 this.executeEvent("onClick", e);
               }}"
-              style=${styleMap({
-                ...this.getStyles(),
-                "min-height": this.childrenComponents.length ? "auto" : "300px",
-              })}
-              class=${classMap({
-                container: true,
-                vertical: this.inputHandlersValue.direction === "vertical",
-                horizontal: this.inputHandlersValue.direction !== "vertical",
-                boxed: this.inputHandlersValue.layout == "boxed",
-              })}
             >
               ${this.childrenComponents.length
                 ? renderComponent(this.childrenComponents.map((component) => ({ ...component, item: this.item })), this.item, this.isViewMode, {...this.component, uniqueUUID : this.uniqueUUID})
@@ -124,7 +179,7 @@ export class VerticalContainer extends BaseElementBlock {
                       </drag-wrapper>
                     </div>
                   `}
-            </div>
+            </nr-container>
           `}
     `;
   }
