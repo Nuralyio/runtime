ad73a324950c97f7ac154f12f72db3dcb64422d4
Aymen <labidi@aymen.co>
2026-01-08 17:03:54 +0100

    feat: Implement presence tracking with PresenceIndicator component and related state management

---
+++ presence/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Presence Module
+ * Real-time presence tracking for collaborative editing
+ */
+
+export * from './presence-types';
+export * from './presence-store';
+export { presenceClient } from './presence-client';
+export { PresenceIndicator } from './PresenceIndicator';
