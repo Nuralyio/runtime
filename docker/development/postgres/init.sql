19bf0897b28eeb84e0b6f2f0d3f72b32f31f4c84
Aymen <labidi@aymen.co>
2026-01-14 18:14:42 +0100

    Add workflows and kv services to Docker Compose; update database initialization and subproject commits

--- docker/development/postgres/init.sql
+++ docker/development/postgres/init.sql
@@ -11,10 +11,18 @@ WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'functions_dev')\gexec
 SELECT 'CREATE DATABASE keycloak'
 WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'keycloak')\gexec
 
+SELECT 'CREATE DATABASE workflows_dev'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'workflows_dev')\gexec
+
+SELECT 'CREATE DATABASE kv_dev'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'kv_dev')\gexec
+
 -- Grant privileges to postgres user on all databases
 GRANT ALL PRIVILEGES ON DATABASE nuraly_dev TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE functions_dev TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE workflows_dev TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE kv_dev TO postgres;
 
 -- Switch to nuraly_dev database and restore backup
 \c nuraly_dev;
