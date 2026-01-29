93b2e839e78e7d9fd4adb75177eddd14e222e73a
Aymen <labidi@aymen.co>
2026-01-29 12:34:25 +0100

    Add database migration and seeding scripts; update Makefile and SQL initialization

--- docker/production/postgres/00-init-databases.sql
+++ docker/production/postgres/00-init-databases.sql
@@ -1,7 +1,20 @@
--- Create additional databases needed by services
+-- Create databases for production - schema managed by migrations (Prisma + Flyway)
+-- This script is executed when the PostgreSQL container is first initialized
+
+-- Create databases
 CREATE DATABASE keycloak;
 CREATE DATABASE functions_prod;
+CREATE DATABASE workflows_prod;
+CREATE DATABASE kv_prod;
+CREATE DATABASE journal_prod;
+-- nuraly_prod is created by default or via environment variables
 
--- Grant privileges
+-- Grant privileges to postgres user
 GRANT ALL PRIVILEGES ON DATABASE keycloak TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE functions_prod TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE workflows_prod TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE kv_prod TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE journal_prod TO postgres;
+
+-- Note: Schema creation is handled by migrations
+-- Run migrations before deploying services: ./scripts/migrate.sh prod
